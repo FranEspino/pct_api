9447d19402961081a3c8816b75e665aa27c410bc
Fran Espino <63974005+FranEspino@users.noreply.github.com>
2022-01-10 08:37:13 -0500

    Update investigation.ts

--- routes/investigation.ts
+++ routes/investigation.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { check } from "express-validator";
 import { existsInvestigator, existsAsesor } from "../helpers/db-validators";
 import { validateFields } from "../middlewares/validate-fields";
+
 import fileUpload from "../middlewares/file-multer";
 import {
   postInvestigation,
@@ -9,7 +10,8 @@ import {
   putInvestigations,
   putDetalleInvestigacion,
   putArchivo,
-  putArchivo64
+  putArchivo64,
+  getDetalleHistorial
 } from "../controllers/investigation";
 
 const router = Router();
@@ -33,6 +35,7 @@ router.get("/", getInvestigations);
 
 router.put("/", fileUpload, putInvestigations);
 router.put("/detalle", putDetalleInvestigacion);
+router.get("/detalle/historial/:id_investigacion", getDetalleHistorial);
 export default router;
 /* 
     url_archivo,
